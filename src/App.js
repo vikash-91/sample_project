6ae2a7bd30967421bc43bb240e8cd140b604ead6
imvikash56 <vikashk@suntechnologies.com>
2025-07-09 12:25:06 +0530

    new branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         this is branch 09-07-2022
+         
         </p>
         <a
           className="App-link"
